e07261ffbd9d6f465cbaf6fb4b8ac6073a51b620
fletcherdeal92 <30945271+fletcherdeal92@users.noreply.github.com>
2025-02-09 11:31:47 -0500

    Solved Lesson Three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // take the string and make it an array
+    let string = '';
+    let letters = [];
+    for (i = 0; i < input.length; i++) {
+        letters.push(input[i]);
+    }
+    //console.log(letters);
 
+    // reverse the array
+    letters.reverse();
+    //console.log(letters);
+
+    // join the array back into a string
+    for (i = 0; i < letters.length; i++) {
+        string += letters[i];
+    }
+    console.log(string);
+    return string;
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
