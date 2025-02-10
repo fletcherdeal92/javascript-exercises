f1b8bed807fef2a337c0a843af15589009ce1aa3
fletcherdeal92 <30945271+fletcherdeal92@users.noreply.github.com>
2025-02-09 19:37:41 -0500

    Solved Problem Four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    if (!Array.isArray(array)) {
+        console.log(`This isn't an array Error.`)
+    } else; 
+
+    return array.filter(x => !args.includes(x))
+    
+    
+   
+
+
 };
+
+let arrayNum = [1, 2, 3, 4, 5];
+let arrayStr = ['a', 'b', 'c', 'd', 'e', 'f', 'apfels'];
+//removeFromArray('hello', 5);
+//removeFromArray(arrayNum, 3)
+removeFromArray(arrayStr, 'f');
 
 // Do not edit below this line
 module.exports = removeFromArray;
